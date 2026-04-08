67f2db5e55b1e437473d477a4655ca551919ff68
Chinmay Spatel <spatelchinmay30@gmail.com>
2026-04-08 18:50:59 +0530

    Added multi-client support using threading

--- master_server.py
+++ master_server.py
@@ -1,38 +1,46 @@
 import socket
 import os
+import threading
 from converter import convert_file
 
 HOST = '127.0.0.1'
 PORT = 5000
 
 os.makedirs("converted", exist_ok=True)
+os.makedirs("test_files", exist_ok=True)
+
+def handle_client(conn, addr):
+    print(f"Connected to {addr}")
+
+    try:
+        filename = conn.recv(1024).decode()
+        data = conn.recv(4096).decode()
+
+        input_path = f"test_files/{filename}"
+        output_path = f"converted/converted_{filename}"
+
+        with open(input_path, "w") as f:
+            f.write(data)
+
+        success = convert_file(input_path, output_path)
+
+        if success:
+            conn.send("File converted successfully".encode())
+        else:
+            conn.send("Conversion failed".encode())
+
+    except Exception as e:
+        print("Error:", e)
+
+    conn.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(5)
 
-print("Server started... Waiting for client...")
+print("Server running...")
 
 while True:
     conn, addr = server.accept()
-    print(f"Connected to {addr}")
-
-    filename = conn.recv(1024).decode()
-    data = conn.recv(4096).decode()
-
-    input_path = f"test_files/{filename}"
-    output_path = f"converted/converted_{filename}"
-
-    os.makedirs("test_files", exist_ok=True)
-
-    with open(input_path, "w") as f:
-        f.write(data)
-
-    success = convert_file(input_path, output_path)
-
-    if success:
-        conn.send("File converted successfully".encode())
-    else:
-        conn.send("Conversion failed".encode())
-
-    conn.close()
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
